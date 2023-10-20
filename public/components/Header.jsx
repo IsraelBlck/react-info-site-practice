f982407bff1ed329ac44199c75febb77bc393262
IsraelBlck <Israelonoja21@gmail.com>
2023-10-20 03:11:39 +0200

    first commit

---
+++ public/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return (
+            <nav>
+                 <img src="../images/reactjs-icon.png" alt="React-logo" />
+                 <h3>ReactFacts</h3>
+                 <h4>React Course - Project 1</h4>
+            </nav>
+    )
+}
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from "react-dom"
+import Header from '../public/components/Header'
+import MainContent from '../public/components/MainContent'
+import Footer from '../public/components/Footer'
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
